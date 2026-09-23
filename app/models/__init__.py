34c7411e5d4403aca8ae29c30ebf84f4fff71450
Danik0baby <app.store01.10.80@gmail.com>
2026-09-23 15:40:00 +0500

    Export chat attachment model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.models.base import Base
+from app.models.attachment import ChatAttachment
 from app.models.chat import ChatSession, Message
 from app.models.product import Product
 from app.models.temporary import IdempotencyKey, PendingOffer
 
-__all__ = ["Base", "ChatSession", "Message", "Product", "PendingOffer", "IdempotencyKey"]
+__all__ = ["Base", "ChatAttachment", "ChatSession", "Message", "Product", "PendingOffer", "IdempotencyKey"]

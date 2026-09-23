384ad56f5cf2b61717714b8488e4d9066f1c2066
Danik0baby <app.store01.10.80@gmail.com>
2026-09-23 15:15:24 +0500

    Add offer workflow errors

--- app/services/errors.py
+++ app/services/errors.py
@@ -8,3 +8,13 @@ class ApplicationError(Exception):
 class ResourceNotFound(ApplicationError):
     code = "not_found"
     status_code = 404
+
+
+class OfferConflict(ApplicationError):
+    code = "offer_conflict"
+    status_code = 409
+
+
+class OfferPreconditionFailed(ApplicationError):
+    code = "offer_precondition_failed"
+    status_code = 409
